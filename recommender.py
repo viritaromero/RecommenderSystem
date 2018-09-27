80231f84fd378df55df730509a8b47ed004b154b
Viridiana Romero <31528909+viritaromero@users.noreply.github.com>
2018-09-26 22:28:37 -0500

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -1,36 +1,54 @@
-# Import Pandas
+#Import Pandas
 import pandas as pd
 
-# Create a Python dataset from a CSV file
-metadata = pd.read_csv('movies_metadata.csv', low_memory=False)
+#Load Videogames Dataset
+videogames = pd.read_csv('Videogames2016.csv', low_memory=False)
 
-# Print the first five rows
-metadata.head(5)
+#Print the first five rows
+videogames.head(5)
+
+#Replace empty values with 0
+
+videogames.fillna(0, inplace=True)
+
+#Replace 'tbd' string with 0
+videogames.replace('tbd',0)
+
+#Check data type for all columns
+videogames.dtypes
+
+#change User_Score data type from Object to numeric
+
+videogames['User_Score'] = pd.to_numeric(videogames['User_Score'])
+
+#Show the first 10 videogames
+
+videogames.head(10)
 
 # Calculate C
-C = metadata['vote_average'].mean()
+C = videogames['User_Score'].mean()
 print(C)
 
-# Calculate the minimum number of votes required to be in the chart, m
-m = metadata['vote_count'].quantile(0.90)
+# Calculate the minimum number of User's votes required to be in the chart, m
+m = videogames['User_Count'].quantile(0.90)
 print(m)
 
-# Filter out all qualified movies into a new DataFrame
-q_movies = metadata.copy().loc[metadata['vote_count'] >= m]
-q_movies.shape
+# Filter out all qualified Video Games into a new DataFrame
+q_videogames = videogames.copy().loc[videogames['User_Count'] >= m]
+q_videogames.shape
 
-# Function that computes the weighted rating of each movie
+# Function that computes the weighted rating of each Videogame
 def weighted_rating(x, m=m, C=C):
-    v = x['vote_count']
-    R = x['vote_average']
-    # Calculation based on the IMDB formula
+    v = x['User_Count']
+    R = x['User_Score']
+    # Calculation
     return (v/(v+m) * R) + (m/(m+v) * C)
-    
+
 # Define a new feature 'score' and calculate its value with 'weighted_rating()'
-q_movies['score'] = q_movies.apply(weighted_rating, axis=1)
+q_videogames['score'] = q_videogames.apply(weighted_rating, axis=1)
 
-#Sort movies based on score calculated above
-q_movies = q_movies.sort_values('score', ascending=False)
+#Sort videogames based on score calculated above
+q_videogames = q_videogames.sort_values('score', ascending=False)
 
-#Print the top 30 movies
-q_movies[['title', 'vote_count', 'vote_average', 'score']].head(30)
+#Print the top 30 videogames
+q_videogames[['Name', 'User_Count', 'User_Score', 'score']].head(30)
